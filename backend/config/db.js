f654f63900d9a53c69917953f98d67f79bd97618
Carolina Gonzalez <36678146+cgonzalez21@users.noreply.github.com>
2020-05-01 02:20:33 -0500

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,9 @@
 module.exports = function () {
 	return {
 		server: '',
-		user: 'sa',
-		port: 1451,
-		password: 'Pa55w0rd!',
-		database: 'REPOS'
+		user: '',
+		port: ,
+		password: '',
+		database: ''
 	}
 }
